73bd6efac8e6b7d868c7e2d707d4bcdff23c94dc
sachag678 <sacha.gunaratne@gmail.com>
2017-03-08 15:09:13 -0500

    Update the SimpleKNN.java with added functionailty

--- src/org/jLOAF/reasoning/SimpleKNN.java
+++ src/org/jLOAF/reasoning/SimpleKNN.java
@@ -1,5 +1,7 @@
 package org.jLOAF.reasoning;
 
+import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.List;
 
 import org.jLOAF.Reasoning;
@@ -21,8 +23,45 @@ public class SimpleKNN implements Reasoning {
 	@Override
 	public Action selectAction(Input i) {
 		List<Case> nn = ret.retrieve(i);
-		//FIXME I should do something in the case k > 1
-		return nn.get(0).getAction();
+		
+		return mostLikelyAction(nn);
+	}
+	
+	public Action mostLikelyAction(List<Case> nn){
+		Hashtable<Action, Integer> nnactions = new Hashtable<Action, Integer>();
+		
+		for(int i =0;i<nn.size();i++){
+			if(!nnactions.containsKey(nn.get(i).getAction())){//hashtable to account for number of times an action is chosen
+				nnactions.put(nn.get(i).getAction(), 1);
+			}else{
+				int value = (int) nnactions.get(nn.get(i).getAction());
+				nnactions.put(nn.get(i).getAction(), value+1);
+			}
+		}
+		return max(nnactions);
+	}
+	
+	/**
+	 * Sacha 2017 Mar
+	 * Calculates the msot likely action given a matrix of actions and counts
+	 * 
+	 * **/
+	public Action max(Hashtable<Action, Integer> h){
+		Enumeration<Action> actions;
+		
+		actions = h.keys();
+		int max = 0;
+		Action action = null;
+		
+		while(actions.hasMoreElements()){
+			Action val = (Action) actions.nextElement();
+			if((int) h.get(val)>max){
+				max = (int) h.get(val);
+				action = val;
+			}
+		}
+		
+		return action;
 	}
 
 }
