4229d552dd3cd61cf5bd16f3389cc7cb2a22c03b
sachag678 <sacha.gunaratne@gmail.com>
2017-03-09 12:00:53 -0500

    Added Comments to kNN.java

--- src/org/jLOAF/retrieve/kNN.java
+++ src/org/jLOAF/retrieve/kNN.java
@@ -20,16 +20,23 @@ public class kNN implements Retrieval {
 	
 	@Override
 	public List<Case> retrieve(Input i) {
+		//creates a new array of distance objects
 		Distance [] dist = new Distance[cb.getSize()];
 		
 		int count =0;
 		
+		//creates a new distance object with the distance between the current case and the case in the case base, as well as the case in the case base
 		for(Case c: cb.getCases()){
 			dist[count] = new Distance(i.similarity(c.getInput()),c);
+			count++;
 		}
 		
+		//sorts the array in descending order
 		Arrays.sort(dist);
 		
+		//could use a sorting algorithm to sort an arraylist of cases in order
+		
+		//gets the k closest neighbours
 		List<Case> best = new ArrayList<Case>();
 		for(int ii=0;ii<k;ii++) best.add(dist[ii].getCase());
 		
