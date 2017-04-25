862759df2416fc05440bc804eb1e1c3f89ad1b83
sachag678 <sacha.gunaratne@gmail.com>
2017-04-25 16:23:34 -0400

    added new class for matching - Hungarian
    started the hungarian - generated the distance matrix - now apply
    hungarian

---
+++ src/org/jLOAF/sim/complex/Hungarian_alg.java
@@ -0,0 +1,56 @@
+package org.jLOAF.sim.complex;
+
+import java.util.Set;
+
+import org.jLOAF.inputs.ComplexInput;
+import org.jLOAF.inputs.Input;
+import org.jLOAF.sim.SimilarityMetricStrategy;
+/***
+ * Sacha Gunaratne 2017 May
+ * 
+ * Uses the hungarian matching algorithm to calculate a minimum dist matching between two sets 
+ * the sets can be of the same size or not. 
+ * 
+ * ***/
+
+public class Hungarian_alg implements SimilarityMetricStrategy {
+
+	@Override
+	public double similarity(Input i1, Input i2) {
+		
+		if(!(i1 instanceof ComplexInput) || !(i2 instanceof ComplexInput) ){
+			throw new IllegalArgumentException("Mean.similarity(..): Not ComplexInputs");
+		}
+		
+		ComplexInput cplx1 = (ComplexInput)i1;
+		ComplexInput cplx2 = (ComplexInput)i2;
+		
+		Set<String> keys = cplx1.getChildNames();
+		Set<String> keys2 = cplx2.getChildNames();
+		
+		int row = 0;
+		int col = 0;
+		double sim_matrix [][];
+		
+		if(keys.size()>=keys2.size()){
+			sim_matrix = new double [keys.size()][keys.size()];
+		}else{
+			sim_matrix = new double [keys2.size()][keys2.size()];
+		}
+		
+		//creates the sim matrix with the keys are rows and keys2 as col
+		for (String s: keys){
+			for (String s1: keys2){
+				sim_matrix[row][col] = cplx1.get(s).similarity(cplx2.get(s1));
+				col++;
+			}
+			row++;
+		}
+		
+		//run Hungarian matching algorithm to calculate minimum dist 
+		
+		
+		return 0;
+	}
+
+}
