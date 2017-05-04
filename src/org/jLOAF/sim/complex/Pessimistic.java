573e1f76bd6868d6a7998af301ca1e89bffb1349
sachag678 <sacha.gunaratne@gmail.com>
2017-05-04 12:17:11 -0400

    Implemented three new similarityMetrics

---
+++ src/org/jLOAF/sim/complex/Pessimistic.java
@@ -0,0 +1,50 @@
+package org.jLOAF.sim.complex;
+
+import java.util.Set;
+
+import org.jLOAF.inputs.ComplexInput;
+import org.jLOAF.inputs.Input;
+import org.jLOAF.sim.SimilarityMetricStrategy;
+
+/***
+ * Returns the most pessimistic estimate for the similarity. (The min similarity)
+ * ***/
+public class Pessimistic implements SimilarityMetricStrategy {
+	@Override
+	public double similarity(Input i1, Input i2) {
+		
+		if(i1== null || i2 == null) return 0.0;
+		
+		if(!(i1 instanceof ComplexInput) || !(i2 instanceof ComplexInput) ){
+			throw new IllegalArgumentException("Mean.similarity(..): Not ComplexInputs");
+		}
+		
+		ComplexInput cplx1 = (ComplexInput)i1;
+		ComplexInput cplx2 = (ComplexInput)i2;
+		
+		Set<String> keys = cplx1.getChildNames();
+		Set<String> keys2 = cplx2.getChildNames();
+		
+		double min = 0;
+		double sim =0;
+		
+		//dealing with mismatched sets with a penalty for not having a paired element
+		if (keys.size()>=keys2.size()){
+			for(String s: keys){
+				if (cplx2.get(s)!=null){
+					sim = cplx1.get(s).similarity(cplx2.get(s));
+					if(min>sim)min=sim;
+				}
+			}
+		}else if(keys.size()<keys2.size()){
+			for(String s: keys2){
+				if (cplx1.get(s)!=null){
+					sim = cplx2.get(s).similarity(cplx1.get(s));
+					if(min>sim)min=sim;
+				}		
+			}
+		}
+		
+		return min;
+	}
+}
