8b0bb1f52c7e6f9ebafbd039f7997f1ba5f6bac3
sachag678 <sacha.gunaratne@gmail.com>
2017-05-09 14:55:01 -0400

    Created an abstract class that can be used to apply weights to similarityMeasures

---
+++ src/org/jLOAF/sim/SimilarityMeasure.java
@@ -0,0 +1,38 @@
+package org.jLOAF.sim;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.jLOAF.weights.Weights;
+
+public abstract class SimilarityMeasure {
+	
+	protected HashMap<String,Double> similarities;
+	protected Weights feat_weights;
+	
+	public SimilarityMeasure(Weights featureweights){
+		similarities = new HashMap<String, Double>();
+		feat_weights = featureweights;
+	}
+	
+	/***
+	 * Calculates the weighted similarity based the features weights
+	 * adds to the total and divides by the size of the keyset()
+	 * 	 * ***/
+	public double calculateWeightedSimilarity(){
+		double totalDistance = 0.0;
+		
+		List<String> featureTypes = new ArrayList<String>(similarities.keySet());
+		for(String currentFeature : featureTypes){
+			double featureDistance = similarities.get(currentFeature);
+				
+			//apply the weight
+			double currentWeight = this.feat_weights.getWeight(currentFeature);
+			totalDistance += featureDistance*currentWeight;
+		}
+		
+		return totalDistance/similarities.keySet().size();
+		
+	}
+}
