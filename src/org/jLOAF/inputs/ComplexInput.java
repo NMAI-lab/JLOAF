6758160b63591c212ecddc50c5c0972eb40a365c
sachag678 <sacha.gunaratne@gmail.com>
2017-05-17 10:57:42 -0400

    Changed the structure of the inputs
    The similarity method in input was implemented and the static
    similaritystrategies in atomic and complex were removed. Now they have
    to be set for every input created.

--- src/org/jLOAF/inputs/ComplexInput.java
+++ src/org/jLOAF/inputs/ComplexInput.java
@@ -13,8 +13,6 @@ public class ComplexInput extends Input {
 
 	private Map<String,Input> collect;
 	
-	private static SimilarityMetricStrategy s_simstrategy;
-	
 	public ComplexInput(String name) {
 		super(name);
 		collect = new HashMap<String,Input>();
@@ -30,38 +28,6 @@ public class ComplexInput extends Input {
 	
 	public Set<String> getChildNames(){
 		return collect.keySet();
-	}
-	
-	@Override
-	public double similarity(Input i) {
-		//See if the user has defined similarity for each specific input, for all inputs
-		//  of a specific type, of defered to superclass
-		if(this.simStrategy != null){
-			return simStrategy.similarity(this, i);
-		}else if(ComplexInput.isClassStrategySet()){
-			return ComplexInput.similarity(this, i);
-		}else{
-			//normally we would defer to superclass, but super
-			// is abstract
-			System.err.println("Problem. In ComplexInput no similarity metric set!");
-			return 0;
-		}
-	}
-
-	private static double similarity(Input complexInput, Input i) {
-		return ComplexInput.s_simstrategy.similarity(complexInput, i);
-	}
-
-	public static boolean isClassStrategySet(){
-		if(ComplexInput.s_simstrategy == null){
-			return false;
-		}else{
-			return true;
-		}
-	}
-
-	public static void setClassStrategy(ComplexSimilarityMetricStrategy s){
-		ComplexInput.s_simstrategy = s;
 	}
 	
 }
