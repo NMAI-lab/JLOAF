7156f18c40cd83c917ca57ee7101353880a3b448
sachag678 <sacha.gunaratne@gmail.com>
2017-05-17 16:20:00 -0400

    Unit Test for Jacard SimilarityMetricStrategy
    The test applies Jacard on two different sized sets of ComplexInputs.

---
+++ src/tests/junit/sim/complex/JacardTest.java
@@ -0,0 +1,62 @@
+package tests.junit.sim.complex;
+
+import static org.junit.Assert.assertEquals;
+
+import org.jLOAF.inputs.AtomicInput;
+import org.jLOAF.inputs.ComplexInput;
+import org.jLOAF.inputs.Feature;
+import org.jLOAF.sim.ComplexSimilarityMetricStrategy;
+import org.jLOAF.sim.SimilarityMetricStrategy;
+import org.jLOAF.sim.atomic.EuclideanDistance;
+import org.jLOAF.sim.complex.AuctionMaximalMatching;
+import org.jLOAF.sim.complex.Jacard;
+import org.jLOAF.sim.complex.Mean;
+import org.junit.Test;
+
+public class JacardTest {
+	/***
+	 * The size of the intersection between flags and flags1 is 3
+	 * The size of flag is 6 and the size of flags1 is 5
+	 * so the solution is 3/(5+6-3) = 3/8 = 0.375
+	 * In this instance there is no requirement for the comparison of the atomicInputs because all it looks at is the size of the sets and the intersections
+	 * between the two sets
+	 * ***/
+	@Test
+	public void TestJacard(){
+		
+		ComplexSimilarityMetricStrategy simMetric1 = new Jacard();
+		ComplexSimilarityMetricStrategy simMetric2 = new Mean();
+		
+		ComplexInput flag1 = new ComplexInput("flag1",simMetric2);
+		ComplexInput flag2 = new ComplexInput("flag2", simMetric2);
+		ComplexInput flag5 = new ComplexInput("flag5", simMetric2);
+		ComplexInput flag7 = new ComplexInput("flag7", simMetric2);
+		ComplexInput flag6 = new ComplexInput("flag6", simMetric2);
+		ComplexInput flag4 = new ComplexInput("flag4", simMetric2);
+
+		
+		ComplexInput flags = new ComplexInput("flag", simMetric1);
+		flags.add(flag1);
+		flags.add(flag2);
+		flags.add(flag7);
+		flags.add(flag6);
+		flags.add(flag4);
+		flags.add(flag5);
+
+		ComplexInput flag3 = new ComplexInput("flag3",simMetric2);
+		ComplexInput flag8 = new ComplexInput("flag4", simMetric2);
+		ComplexInput flag9 = new ComplexInput("flag7", simMetric2);
+		ComplexInput flag10 = new ComplexInput("flag9", simMetric2);
+		ComplexInput flag11 = new ComplexInput("flag5", simMetric2);
+		
+		ComplexInput flags1 = new ComplexInput("flag", simMetric1);
+		flags1.add(flag3);
+		flags1.add(flag8);
+		flags1.add(flag9);
+		flags1.add(flag10);
+		flags1.add(flag11);
+		
+		assertEquals(flags.similarity(flags1),0.375,0.001);
+		
+	}
+}
