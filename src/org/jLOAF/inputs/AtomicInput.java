98996a3b8cb5c7e64bf9427780e8bbec419d0aee
sachag678 <sacha.gunaratne@gmail.com>
2017-05-17 11:39:47 -0400

    The user has to pass the similarityMetric when creating a new Input

--- src/org/jLOAF/inputs/AtomicInput.java
+++ src/org/jLOAF/inputs/AtomicInput.java
@@ -4,14 +4,17 @@
  */
 package org.jLOAF.inputs;
 
+import org.jLOAF.sim.SimilarityMetricStrategy;
+
 public class AtomicInput extends Input {
 
 	private static final long serialVersionUID = 1L;
 	
 	protected Feature feat;
 	
-	public AtomicInput(String name, Feature f) {
+	public AtomicInput(String name, Feature f, SimilarityMetricStrategy sim) {
 		super(name);
+		super.setSimilarityMetric(sim);
 		this.feat = f;
 	}
 
