9353b6fe95a6787f2fcd0a00ba379f3879179db6
sachag678 <sacha.gunaratne@gmail.com>
2017-05-17 11:49:13 -0400

    Added similarityMetricStrategy to all inputs in Tests

--- src/org/jLOAF/inputs/complex/Matrix.java
+++ src/org/jLOAF/inputs/complex/Matrix.java
@@ -9,50 +9,19 @@ import org.jLOAF.sim.SimilarityMetricStrategy;
 public class Matrix extends ComplexInput {
 
 	private static final long serialVersionUID = 1L;
-
-	private static SimilarityMetricStrategy simMet;
 	
 	private int rows = 0;
 	private int cols = 0;
 	
-	public Matrix(String name, double[][] matrix) {
-		super(name);
+	public Matrix(String name, double[][] matrix, SimilarityMetricStrategy sim) {
+		super(name,sim);
 		this.rows = matrix.length;
 		this.cols = matrix[0].length;
 		for(int ii = 0; ii < this.rows; ii++){
 			for(int jj = 0; jj < this.cols; jj++){
-				MatrixCell mc = new MatrixCell(ii+"-"+jj, new Feature(matrix[ii][jj]));
+				MatrixCell mc = new MatrixCell(ii+"-"+jj, new Feature(matrix[ii][jj]), sim);
 				this.add(mc);
 			}
 		}
-	}
-
-	@Override
-	public double similarity(Input i) {
-		//See if the user has defined similarity for each specific input, for all inputs
-		//  of a specific type, of defered to superclass
-		if(this.simStrategy != null){
-			return simStrategy.similarity(this, i);
-		}else if(Matrix.isClassStrategySet()){
-			return Matrix.similarity(this, i);
-		}else{
-			return super.similarity(i);
-		}
-	}
-
-	private static double similarity(Input complexInput, Input i) {
-		return Matrix.simMet.similarity(complexInput, i);
-	}
-
-	public static boolean isClassStrategySet(){
-		if(Matrix.simMet == null){
-			return false;
-		}else{
-			return true;
-		}
-	}
-
-	public static void setClassSimilarityMetric(SimilarityMetricStrategy s){
-		Matrix.simMet = s;
 	}
 }
--- src/tests/junit/sim/atomic/PercentDifferenceTest.java
+++ src/tests/junit/sim/atomic/PercentDifferenceTest.java
@@ -2,26 +2,27 @@ package tests.junit.sim.atomic;
 
 import org.jLOAF.inputs.AtomicInput;
 import org.jLOAF.inputs.Feature;
+import org.jLOAF.sim.SimilarityMetricStrategy;
 import org.jLOAF.sim.atomic.PercentDifference;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class PercentDifferenceTest {
 	
+	SimilarityMetricStrategy sim = new PercentDifference();
 	@Test
 	public void TestPDNotSame(){
-		AtomicInput i1 = new AtomicInput("h1",new Feature(1.6));
-		AtomicInput i2 = new AtomicInput("h2",new Feature(1.2));
-		PercentDifference p = new PercentDifference();
-		assertEquals(p.similarity(i1,i2),0.857,0.001);
+		
+		AtomicInput i1 = new AtomicInput("h1",new Feature(1.6),sim);
+		AtomicInput i2 = new AtomicInput("h2",new Feature(1.2),sim);
+		assertEquals(i1.similarity(i2),0.857,0.001);
 	}
 	
 	@Test
 	public void TestPDDividebyZero(){
-		AtomicInput i1 = new AtomicInput("h1",new Feature(1.6));
-		AtomicInput i2 = new AtomicInput("h2",new Feature(-1.6));
-		PercentDifference p = new PercentDifference();
-		assertEquals(p.similarity(i1,i2),0.0,0.001);
+		AtomicInput i1 = new AtomicInput("h1",new Feature(1.6),sim);
+		AtomicInput i2 = new AtomicInput("h2",new Feature(-1.6),sim);
+		assertEquals(i1.similarity(i2),0.0,0.001);
 	}
 	
 }
