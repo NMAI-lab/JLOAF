490299e4f293f660f9952797a066824cd0a79d3e
sachag678 <sacha.gunaratne@gmail.com>
2017-07-03 19:14:42 -0400

    Created new wrapper method for feature selection - using GA

---
+++ src/org/jLOAF/preprocessing/filter/featureSelection/GeneticAlgorithmWeightSelector.java
@@ -0,0 +1,90 @@
+package org.jLOAF.preprocessing.filter.featureSelection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Random;
+
+import org.jLOAF.casebase.Case;
+import org.jLOAF.casebase.CaseBase;
+import org.jLOAF.inputs.ComplexInput;
+import org.jLOAF.preprocessing.filter.CaseBaseFilter;
+import org.jLOAF.sim.complex.WeightedMean;
+import org.jLOAF.weights.SimilarityWeights;
+/**
+ * This method will take a casebase that has some similarityWeights for every feature and determine the best weights for each feature.
+ * The filter will return a casebase with updated weights
+ * @author sachagunaratne
+ * @since 2017 June
+ * 
+ * ***/
+public class GeneticAlgorithmWeightSelector extends CaseBaseFilter {
+	Random r;
+	public GeneticAlgorithmWeightSelector(CaseBaseFilter f) {
+		super(f);
+		r = new Random();
+	}
+
+	@Override
+	public CaseBase filter(CaseBase initial) {
+		Case first = (Case)initial.getCases().toArray()[0];
+		
+		ComplexInput i = null; 
+		try{
+			i = (ComplexInput)first.getInput();
+		}catch(ClassCastException e){
+			System.out.println(e);
+			return initial;
+		}
+		
+		if(i.getSimilarityMetricStrategy() instanceof WeightedMean){
+			SimilarityWeights sim =((WeightedMean)i.getSimilarityMetricStrategy()).getSimilarityWeights();
+
+			double old_accuracy = 0;
+			double new_accuracy = 1;
+			while((new_accuracy-old_accuracy)<1e-4){
+				old_accuracy = new_accuracy;
+				
+				//set similarityfeatures randomly - step 1 GA - initialization
+				int len = sim.getWeightedItems().size();
+				//create a list of lists which will hold the initial weights
+				ArrayList<ArrayList<Double>> listOfWeights = new ArrayList<ArrayList<Double>>();
+				ArrayList<Double> weights1 = new ArrayList<Double>();
+				ArrayList<Double> weights2 = new ArrayList<Double>();
+				ArrayList<Double> weights3 = new ArrayList<Double>();
+				ArrayList<Double> weights4 = new ArrayList<Double>();
+				
+				//initialize the weights for each list
+				for(int ii=0;ii<len;ii++){
+					weights1.add(r.nextDouble());
+					weights2.add(r.nextDouble());
+					weights3.add(r.nextDouble());
+					weights4.add(r.nextDouble());
+				}
+				//add the weights to the list of lists
+				listOfWeights.add(weights1);
+				listOfWeights.add(weights2);
+				listOfWeights.add(weights3);
+				listOfWeights.add(weights4);
+				
+				for(ArrayList<Double> weight: listOfWeights){
+					//set the simFeatures for each set of weights
+					int index = 0;
+					for(String w:i.getChildNames()){
+						sim.setFeatureWeight(w, weight.get(index));
+					}
+					
+				}
+				
+				//calculate fitness - step 2 GA - selection
+				
+				//create reproduction pool - step 2 GA - selection
+				
+				//cross produce - step 4 GA - mating
+				
+				//mutate - step 5 GA - mutation
+			}
+		}
+		return initial;
+	}
+
+}
