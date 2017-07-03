5653506ad3b5162fbe2954ec65f7466ff7ac50ed
sachag678 <sacha.gunaratne@gmail.com>
2017-07-03 19:15:18 -0400

    minor changes

--- src/org/jLOAF/preprocessing/filter/featureSelection/GeneticAlgorithmWeightSelector.java
+++ src/org/jLOAF/preprocessing/filter/featureSelection/GeneticAlgorithmWeightSelector.java
@@ -2,11 +2,17 @@ package org.jLOAF.preprocessing.filter.featureSelection;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Random;
+import java.util.TreeMap;
+import java.util.stream.IntStream;
 
+import org.jLOAF.Agent;
+import org.jLOAF.agents.GenericAgent;
 import org.jLOAF.casebase.Case;
 import org.jLOAF.casebase.CaseBase;
 import org.jLOAF.inputs.ComplexInput;
+import org.jLOAF.performance.Statistics;
 import org.jLOAF.preprocessing.filter.CaseBaseFilter;
 import org.jLOAF.sim.complex.WeightedMean;
 import org.jLOAF.weights.SimilarityWeights;
@@ -38,50 +44,99 @@ public class GeneticAlgorithmWeightSelector extends CaseBaseFilter {
 		
 		if(i.getSimilarityMetricStrategy() instanceof WeightedMean){
 			SimilarityWeights sim =((WeightedMean)i.getSimilarityMetricStrategy()).getSimilarityWeights();
+			
+			//set similarityfeatures randomly - step 1 GA - initialization
+			//-------------------------------------------------------------------------
+			int len = sim.getWeightedItems().size();
+			//create a list of lists which will hold the initial weights
+			ArrayList<ArrayList<Double>> listOfWeights = new ArrayList<ArrayList<Double>>();
+			ArrayList<Double> weights1 = new ArrayList<Double>();
+			ArrayList<Double> weights2 = new ArrayList<Double>();
+			ArrayList<Double> weights3 = new ArrayList<Double>();
+			ArrayList<Double> weights4 = new ArrayList<Double>();
+			
+			//contains the fitness for each weight
+			ArrayList<Float> fitness = new ArrayList<Float>();
+			
+			//initialize the weights for each list
+			for(int ii=0;ii<len;ii++){
+				weights1.add(r.nextDouble());
+				weights2.add(r.nextDouble());
+				weights3.add(r.nextDouble());
+				weights4.add(r.nextDouble());
+			}
+			//add the weights to the list of lists
+			listOfWeights.add(weights1);
+			listOfWeights.add(weights2);
+			listOfWeights.add(weights3);
+			listOfWeights.add(weights4);
 
 			double old_accuracy = 0;
 			double new_accuracy = 1;
 			while((new_accuracy-old_accuracy)<1e-4){
 				old_accuracy = new_accuracy;
 				
-				//set similarityfeatures randomly - step 1 GA - initialization
-				int len = sim.getWeightedItems().size();
-				//create a list of lists which will hold the initial weights
-				ArrayList<ArrayList<Double>> listOfWeights = new ArrayList<ArrayList<Double>>();
-				ArrayList<Double> weights1 = new ArrayList<Double>();
-				ArrayList<Double> weights2 = new ArrayList<Double>();
-				ArrayList<Double> weights3 = new ArrayList<Double>();
-				ArrayList<Double> weights4 = new ArrayList<Double>();
 				
-				//initialize the weights for each list
-				for(int ii=0;ii<len;ii++){
-					weights1.add(r.nextDouble());
-					weights2.add(r.nextDouble());
-					weights3.add(r.nextDouble());
-					weights4.add(r.nextDouble());
-				}
-				//add the weights to the list of lists
-				listOfWeights.add(weights1);
-				listOfWeights.add(weights2);
-				listOfWeights.add(weights3);
-				listOfWeights.add(weights4);
 				
+				Agent a = new GenericAgent();
+				Statistics st = new Statistics(a);
+				
+				//calculate fitness - step 2 GA - selection
+				//---------------------------------------------------------
 				for(ArrayList<Double> weight: listOfWeights){
 					//set the simFeatures for each set of weights
 					int index = 0;
 					for(String w:i.getChildNames()){
 						sim.setFeatureWeight(w, weight.get(index));
 					}
+					//train on new sim feature weights
+					a.train(initial);
+					
+					//test the prediction capability
+					for(Case test: initial.getCases()){
+						st.predictedCorrectActionName(test);
+					}
+					//add accuracy to fitness function
+					fitness.add(st.getClassificationAccuracy());
 					
 				}
 				
-				//calculate fitness - step 2 GA - selection
-				
 				//create reproduction pool - step 2 GA - selection
+				//--------------------------------------------------------
 				
-				//cross produce - step 4 GA - mating
+				//choose the two best weights using the fitness function
 				
-				//mutate - step 5 GA - mutation
+				//choose the best two fitness functions
+				Object[] temp = fitness.toArray();
+				float [] fitness_array = new float [temp.length];
+				for(int lenght=0;lenght<temp.length;lenght++){
+					fitness_array[lenght] = (float)temp[lenght];
+				}
+				Map<Float, Integer> map = new TreeMap<Float, Integer>();
+			    for (int i2 = 0; i2 < fitness_array.length; ++i2) {
+			        map.put(fitness_array[i2], i2);
+			    }
+			    Collection<Integer> indices = map.values();
+				
+			    Object [] indexes1 = indices.toArray(); 
+			    int [] indexes = new int [temp.length];
+			    for(int lenght=0;lenght<temp.length;lenght++){
+			    	indexes[lenght] = (int)indexes1[lenght];
+				}
+			    
+			    
+			    ArrayList<ArrayList<Double>> mating_pool =  new ArrayList<ArrayList<Double>>();
+			    //add two of the best weight lists to the mating pool
+			    for(int counter1=0;counter1<2;counter1++){
+			    	mating_pool.add(listOfWeights.get(indexes[counter1]));
+			    }
+			    
+			    //cross produce - step 3 GA - mating
+			    //----------------------------------------------------------------------------
+			    
+				
+				//mutate - step 3 GA - mutation
+			    //----------------------------------------------------------------------------
 			}
 		}
 		return initial;
