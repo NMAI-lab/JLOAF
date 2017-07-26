c6798715db35684c428743bc5cab1f1cee61cde3
sachag678 <sacha.gunaratne@gmail.com>
2017-07-26 14:33:29 -0400

    Updated writer

--- src/org/jLOAF/util/writer.java
+++ src/org/jLOAF/util/writer.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class writer {
 	HashMap <String, List<String>> container = new HashMap<String, List<String>>();
 	String [] agents = {"MultipleSequenceAgent", "WallFollowerAgent", "ZigZagAgent","PreviousThreeStatesDependentAgent","SmartSquareAgent"};
-	String agentName = agents[4];
+	String agentName = agents[0];
 	String [] traceNames = {"trace-m0-","trace-m1-","trace-m2-","trace-m3-","trace-m4-"};
 	String fileext = ".txt ";
 	String output_file = "";
@@ -20,24 +20,29 @@ public class writer {
 	String [] cbf = {"fullclustering", "sampling", ""};
 	//String [] cbf2 = {"", "hillclimbing", "geneticAlgorithm", "sequentialBackwardsAlgorithm", "weightsSeperator"};
 	String [] cbf2 = {""};
-	String loc = "batch_files/"+agentName+"/";
+	String loc = "C:/Users/sachagunaratne/Documents/GitHub/batch_files/";
 	
 	public void writeReactive() {
 		int count = 0;
 		try {
 			Files.createDirectories(Paths.get(loc.substring(0, loc.length()-1)));
-			FileWriter master = new FileWriter(loc+agentName+"master.bat");
-			FileWriter fw = new FileWriter(loc+agentName+"0.bat");
-			master.write("start /wait cmd /k CALL "+ agentName+"0.bat");
+			FileWriter master = new FileWriter(loc+agentName+"/"+agentName+"master.bat");
+			FileWriter fw = new FileWriter(loc+agentName+"/"+agentName+"0.bat");
+			fw.write("(");
+			fw.write("\r\n");
+			master.write("CALL "+ agentName+"0.bat");
 			master.write("\r\n");
 			for(int i=0;i<reasoners.length;i++) {
 				for(int k = 0;k<cbf.length;k++) {
 					for(int l =0;l<cbf2.length;l++) {
 						if(count%4==0 && count!=0) {
+							fw.write(") | set /P  \"=\"");
+							fw.write("\r\n");
+							fw.write("CALL "+ agentName+String.valueOf(count/4)+".bat");
 							fw.flush();
-							fw = new FileWriter(loc+agentName+String.valueOf(count/4)+".bat");
-							master.write("start /wait cmd /k CALL "+ agentName+String.valueOf(count/4)+".bat");
-							master.write("\r\n");
+							fw = new FileWriter(loc+agentName+"/"+agentName+String.valueOf(count/4)+".bat");
+							fw.write("(");
+							fw.write("\r\n");
 						}
 					String s1 = "start java -jar vacuum.jar files ";
 					fw.write(s1);
@@ -46,7 +51,7 @@ public class writer {
 						fw.write(s2);
 					}
 					fw.write("files ");
-					fw.write("output " + reasoners[i]+"_"+agentName+"_"+cbf[k]+"_"+cbf2[l]+".csv" +" output ");
+					fw.write("output " + "Results/"+reasoners[i]+"_"+agentName+"_"+cbf[k]+"_"+cbf2[l]+".csv" +" output ");
 					
 					//accounts for empty filter
 					if(cbf[k].equals("") & !cbf2[l].equals("")) {
@@ -68,6 +73,9 @@ public class writer {
 				}
 				}
 			}
+			fw.write(") | set /P  \"=\"");
+			fw.write("\r\n");
+			fw.write("EXIT");
 			fw.close();
 			master.close();
 		} catch (IOException e) {
@@ -79,34 +87,49 @@ public class writer {
 	public static void main(String []a) {
 		writer w = new writer();
 		for(int i=0;i<5;i++){
-		
+			w.setAgent(i);
 		if (i!=1) {
-			w.stateBased = true;
-			w.writeReactive();
+			w.stateBased = true;		
+			w.writeState();
 		}else {
 			w.stateBased = false;
-			w.writeState();	
+			w.writeReactive();	
 		}
 		}
+//	if(w.stateBased){
+//		w.writeState();
+//	}else{
+//		w.writeReactive();
+//	}
+//	}
 	}
+	public void setAgent(int i){
+		this.agentName = this.agents[i];
+	}
+	
 
 	private void writeState() {
 		int count = 0;
 		try {
 			Files.createDirectories(Paths.get(loc.substring(0, loc.length()-1)));
-			FileWriter master = new FileWriter(loc+agentName+"master.bat");
-			FileWriter fw = new FileWriter(loc+agentName+"0.bat");
-			master.write("start /wait cmd /k CALL "+ agentName+"0.bat");
+			FileWriter master = new FileWriter(loc+agentName+"/"+agentName+"master.bat");
+			FileWriter fw = new FileWriter(loc+agentName+"/"+agentName+"0.bat");
+			fw.write("(");
+			fw.write("\r\n");
+			master.write("start /w "+ agentName+"0.bat");
 			master.write("\r\n");
 			for(int i=0;i<reasoners.length;i++) {
 				for(int k = 0;k<cbf.length;k++) {
 					for(int l =0;l<cbf2.length;l++) {
 						for(int m=0;m<stsim.length;m++) {
-							if(count%4==0) {
+							if(count%4==0 && count!=0) {
+								fw.write(") | set /P  \"=\"");
+								fw.write("\r\n");
+								fw.write("CALL "+ agentName+String.valueOf(count/4)+".bat");
 								fw.flush();
-								fw = new FileWriter(loc+agentName+String.valueOf(count/4)+".bat");
-								master.write("start /wait cmd /k CALL "+ agentName+String.valueOf(count/4)+".bat");
-								master.write("\r\n");
+								fw = new FileWriter(loc+agentName+"/"+agentName+String.valueOf(count/4)+".bat");
+								fw.write("(");
+								fw.write("\r\n");
 							}
 							String s1 = "start java -jar vacuum.jar files ";
 							fw.write(s1);
@@ -115,7 +138,7 @@ public class writer {
 								fw.write(s2);
 							}
 							fw.write("files ");
-							fw.write("output " + reasoners[i]+"_"+agentName+"_"+cbf[k]+"_"+cbf2[l]+".csv" +" output ");
+							fw.write("output " + "Results/"+ reasoners[i]+"_"+agentName+"_"+cbf[k]+"_"+cbf2[l]+"_"+stsim[m]+".csv" +" output ");
 
 							//accounts for empty filter
 							if(cbf[k].equals("") & !cbf2[l].equals("")) {
@@ -138,6 +161,9 @@ public class writer {
 					}
 				}
 			}
+			fw.write(") | set /P  \"=\"");
+			fw.write("\r\n");
+			fw.write("EXIT");
 			fw.close();
 			master.close();
 		} catch (IOException e) {
