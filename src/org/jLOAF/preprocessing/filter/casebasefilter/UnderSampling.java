163d30fdb662551ad9e0757b57cd236cb33921c9
sachag678 <sacha.gunaratne@gmail.com>
2017-07-27 17:07:18 -0400

    Renamed UnderSampling

--- src/org/jLOAF/preprocessing/filter/casebasefilter/UnderSampling.java
+++ src/org/jLOAF/preprocessing/filter/casebasefilter/UnderSampling.java
@@ -10,11 +10,10 @@ import org.jLOAF.inputs.StateBasedInput;
 import org.jLOAF.preprocessing.filter.CaseBaseFilter;
 import org.jLOAF.sim.StateBasedSimilarity;
 
-public class SMOTE extends CaseBaseFilter {
-
-	public SMOTE(CaseBaseFilter f) {
+public class UnderSampling extends CaseBaseFilter {
+	double underSamplePercent = 0;
+	public UnderSampling(CaseBaseFilter f) {
 		super(f);
-		
 	}
 
 	@Override
@@ -24,22 +23,49 @@ public class SMOTE extends CaseBaseFilter {
 		}
 		List<Action> actions = CaseBase.getActionNames(initial);
 		HashMap<String, CaseBase> casebases = new HashMap<String, CaseBase>();
+		HashMap<String, Integer> sizes = new HashMap<String, Integer>();
 		//separates the classes into different casebases
 		for(Action a: actions) {
 			CaseBase temp = new CaseBase();
 			for(Case c: initial.getCases()) {
-				if(c.getAction().equals(a)) {
-					temp.createThenAdd(c.getInput(), c.getAction(),(StateBasedSimilarity)c.getInput().getSimilarityMetricStrategy() );
+				if(c.getAction().getName().equals(a.getName())) {
+					temp.add(c);
 				}
 			}
 			casebases.put(a.getName(),temp);
+			sizes.put(a.getName(),temp.getSize());
 		}
 		
 		//make each casebase the same size? Using oversampling and undersampling
 		//undersampling removes cases from the large cases
 		//oversampling involves making synthetic data using SMOTE
 		
-		return null;
+		//find largest casebase
+				
+		int min = sizes.get(actions.get(0).getName());
+		int index =0;
+		int count =0;
+		for(Action a:actions){
+			if(sizes.get(a.getName())<min){min=sizes.get(a.getName());index=count;}
+			count++;
+		}
+  		String smallest = actions.get(index).getName();
+		double underSamplePercent =0;
+		count=0;
+		for(Action a:actions){
+			if(!a.getName().equals(smallest)){
+				for(Case c: casebases.get(a.getName()).getCases()){
+					initial.remove(c);
+					if(count>=(sizes.get(a.getName())-min)*underSamplePercent){
+						break;
+					}
+					count++;
+				}
+			}
+			count=0;
+		}
+		
+		return initial;
 	}
 
 }
