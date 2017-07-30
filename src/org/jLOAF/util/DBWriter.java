39a9ab9c6f5b5606bc7be55d2e051811961063ac
sachag678 <sacha.gunaratne@gmail.com>
2017-07-29 23:43:42 -0400

    Edited dbwriter to write results to table. only thing left to do is edit writer so that the filename reflects the testcase well enough that DBWriter can convert it into a table.

--- src/org/jLOAF/util/DBWriter.java
+++ src/org/jLOAF/util/DBWriter.java
@@ -20,13 +20,44 @@ public class DBWriter {
 
 			Statement statement = connection.createStatement();
 			statement.setQueryTimeout(30); 
-
-			statement.executeUpdate("CREATE TABLE choco (Performance STRING, Mean STRING, Standard Deviation STRING)");
-
+			
+			//creates mean table
+			StringBuilder sb = new StringBuilder();
 			for(String feature: mean.keySet()){
-				statement.executeUpdate("INSERT INTO choco values(' "+feature+"', '"+mean.get(feature)+"',' "+stdev.get(feature)+"')");
+				sb.append(feature+" STRING,");
 			}
-
+			sb.deleteCharAt(sb.length()-1);
+			
+			StringBuilder sb2 = new StringBuilder();
+			for(String feature: mean.keySet()){
+				sb2.append("'");
+				sb2.append(mean.get(feature));
+				sb2.append("'");
+				sb2.append(",");
+			}
+			sb2.deleteCharAt(sb2.length()-1);
+			
+			statement.executeUpdate("CREATE TABLE IF NOT EXISTS mean (Agent STRING, Reasoner STRING, Filter1 STRING, Filter2 STRING, stateBasedSim STRING, complexSim STRING,"+sb.toString()+")");
+			statement.executeUpdate("INSERT INTO mean values('wf', 'KNN','clustering','none','none','none',"+sb2.toString()+")");
+			
+			//creates std table
+			StringBuilder sb3 = new StringBuilder();
+			for(String feature: mean.keySet()){
+				sb3.append(feature+" STRING,");
+			}
+			sb3.deleteCharAt(sb3.length()-1);
+			
+			StringBuilder sb4 = new StringBuilder();
+			for(String feature: stdev.keySet()){
+				sb4.append("'");
+				sb4.append(stdev.get(feature));
+				sb4.append("'");
+				sb4.append(",");
+			}
+			sb4.deleteCharAt(sb4.length()-1);
+			
+			statement.executeUpdate("CREATE TABLE IF NOT EXISTS standard_dev (Agent STRING, Reasoner STRING, Filter1 STRING, Filter2 STRING, stateBasedSim STRING, complexSim STRING,"+sb3.toString()+")");
+			statement.executeUpdate("INSERT INTO standard_dev values('wf', 'KNN','clustering','none','none','none',"+sb4.toString()+")");
 
 		}catch(SQLException e){  System.err.println(e.getMessage()); 
 
